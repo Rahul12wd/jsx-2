6501d90a3d62c737a8bf0d1eedb39cdf058560cc
Rahul12wd <96861219+Rahul12wd@users.noreply.github.com>
2022-11-01 11:44:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Evaluation of, <br /> Airbnb Frontend </h1>
+
+       <h3>@spikerbrehm</h3> 
+
+       <img src='https://www.citypng.com/public/uploads/preview/-31600816942dfdajpbva4.png' alt="" height='50' />
+       
+    </div>
+  );
+}
+
+export default App;
